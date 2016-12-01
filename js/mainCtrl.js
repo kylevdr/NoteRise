105b2bbc78f8b0f0200537ef2f0d31c3ae656027
Kyle VanDeRiet <kylevdr@gmail.com>
2016-11-30 17:19:06 -0700

    Notes can be deleted. Notes persist between refreshes and browser sesssions. Other improvements.

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -7,27 +7,60 @@ angular.module('noteRiseApp').controller('mainCtrl', function($scope, mainServic
     //     $scope.notes = results;
     // })
 
+    $scope.saveNotes = function() {
+        mainService.setNotes($scope.notes);
+    }
+
     $scope.setNote = function(note) {
         $scope.currentNote = note;
+        $scope.editorNote = {
+            title: $scope.currentNote.title,
+            text: $scope.currentNote.text
+        };
     };
 
     $scope.createNote = function() {
         var newNote = {
             title: "New Note",
             text: "",
-            time: new Date()
+            time: new Date(),
+            id: $scope.notes.length
         };
         $scope.notes.push(newNote);
         $scope.currentNote = newNote;
+        $scope.editorNote = {
+            title: $scope.currentNote.title,
+            text: $scope.currentNote.text
+        };
+        $scope.saveNotes();
+    };
+
+    $scope.saveNote = function() {
+        $scope.currentNote.title = $scope.editorNote.title;
+        $scope.currentNote.text = $scope.editorNote.text;
+        $scope.currentNote.time = new Date();
+        $scope.saveNotes();
     };
 
     $scope.saveNewNote = function() {
         var newNote = {
-            title: $scope.currentNote.title,
-            text: $scope.currentNote.text,
-            time: new Date()
+            title: $scope.editorNote.title,
+            text: $scope.editorNote.text,
+            time: new Date(),
+            id: $scope.notes.length
         };
         $scope.notes.push(newNote);
         $scope.currentNote = newNote;
+        $scope.saveNotes();
+    };
+
+    $scope.archiveNote = function(note) {
+        note.archived = true;
+        $scope.saveNotes();
+    };
+
+    $scope.clearCurrentNote = function() {
+        $scope.currentNote = null;
+        $scope.editorNote = null;
     }
 });
--- js/mainService.js
+++ js/mainService.js
@@ -1,20 +1,20 @@
 angular.module('noteRiseApp').service('mainService', function($q) {
 
-    var notes = [
+    var sampleNotes = [
         {
             title: 'Sample note',
-            text: 'Sample note text',
-            time: '2016-11-29T23:39:38.341Z'
-        },
-        {
-            title: 'Sample note 2',
-            text: 'Sample note text 2',
-            time: '2016-11-29T23:39:38.341Z'
+            text: 'You don\'t have any notes yet. Here is a sample note to get you started.',
+            time: new Date(),
+            id: 0
         }
     ];
 
     this.getNotes = function() {
-        return notes;
-    }
+        return localStorage.getItem('notes') ? JSON.parse(localStorage.getItem('notes')) : sampleNotes;
+    };
+
+    this.setNotes = function(noteList) {
+        localStorage.setItem('notes', JSON.stringify(noteList));
+    };
 
 });
